f54675862229d1189f83ec75f87be4e39628527f
Chiiii318 <483308261@qq.com>
2026-07-01 03:35:11 +0800

    修复bug

--- js_phone/app_wechat.js
+++ js_phone/app_wechat.js
@@ -306,7 +306,7 @@ function renderMe() {
     var pc = typeof playerColor!=='undefined'?playerColor:'#ff9eaa';
     var pWxId = typeof playerWxId!=='undefined'?playerWxId:'player_001';
     
-    return '<div class="wechat-container"><div class="wx-navbar"><div class="wx-navbar-left"></div><div class="wx-navbar-center">我</div><div class="wx-navbar-right"></div></div><div class="wx-body wx-me-page"><div class="wx-me-header" onclick="wxNav(\'myprofile\')">
+        return '<div class="wechat-container"><div class="wx-navbar"><div class="wx-navbar-left"></div><div class="wx-navbar-center">我</div><div class="wx-navbar-right"></div></div><div class="wx-body wx-me-page"><div class="wx-me-header" onclick="wxNav(\'myprofile\')"></div>'+
         '<div class="wx-me-gap"></div>'+
         '<div class="wx-me-item" onclick="wxNav(\'moments\')"><div class="wx-me-item-icon">'+IC.moments+'</div><span class="wx-me-item-name">朋友圈</span><div class="wx-me-item-arrow">'+IC.arrowR+'</div></div>'+
         '<div class="wx-me-gap"></div>'+
